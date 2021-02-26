acd36609e5ca4e99224ed8ce51b94b0c277f4bee
pilotrick <56171872+pilotrick@users.noreply.github.com>
2021-02-26 09:04:01 -0400

    Add files via upload

---
+++ website_warehouse-master/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Website Warehouse',
+    'category': 'Website',
+    'sequence': 55,
+    'summary': 'Select warehouse in checkout page',
+    'website': '',
+    'version': '1.1',
+    'description': "",
+    'depends': ['payment','web','website_sale'],
+    'data': [
+        'views/payment_template.xml',
+        'views/stock_warehouse.xml',
+    ],
+
+
+}
